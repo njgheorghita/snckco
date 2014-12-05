cb96c2dacd71959f0785e414be34d5c809c0429b
nick <njgheorghita@email.wm.edu>
2014-12-05 11:54:13 -0600

    snckco-mean
    building website with mean framework scaffold by yeoman

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('snckcoApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
