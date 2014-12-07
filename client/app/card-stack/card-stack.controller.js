20a496eb947bbee491ec207cd37c09d2db78b8f8
nick <njgheorghita@email.wm.edu>
2014-12-07 14:32:33 -0600

    pics!

--- client/app/card-stack/card-stack.controller.js
+++ client/app/card-stack/card-stack.controller.js
@@ -4,10 +4,38 @@ angular.module('snckcoApp')
 
 .controller('Card-stackCtrl', function ($scope) {
         $scope.cards = [
-            {name: 'clubs', symbol: '♣'},
-            {name: 'diamonds', symbol: '♦'},
-            {name: 'hearts', symbol: '♥'},
-            {name: 'spades', symbol: '♠'}
+         './../../assets/images/banana.jpg',
+         './../../assets/images/bagel.jpg',
+         './../../assets/images/cheesePbCrackers.jpg',
+         './../../assets/images/clifBuildersBar.jpg.jpg',
+         './../../assets/images/crunchBar.jpg',
+         './../../assets/images/doritosNachoCheese.jpg',
+         './../../assets/images/famousAmos.jpg',
+         './../../assets/images/fruitySnacks.jpg',
+         './../../assets/images/gardettos.jpg',
+         './../../assets/images/hersheysAlmonds.jpg',
+         './../../assets/images/hundredCalOreos.jpg',
+         './../../assets/images/laysBbqChips.jpg',
+         './../../assets/images/lifeSaversFruity.jpg',
+         './../../assets/images/lifeSaversMint.jpg',
+         './../../assets/images/m&msPlain.jpg',
+         './../../assets/images/mentosMint.jpg',
+         './../../assets/images/natureValleyOatshoney.jpg',
+         './../../assets/images/plantersPeanuts.jpg',
+         './../../assets/images/popcornersKettle.jpg',
+         './../../assets/images/popTartBrownSugarCinnamon.jpg',
+         './../../assets/images/popTartHotFudgeSundae.jpg',
+         './../../assets/images/reesesPbCups.jpg',
+         './../../assets/images/riceKrispieTreat.jpg',
+         './../../assets/images/roldGoldPretzelsTinyTwist.jpg',
+         './../../assets/images/skittles.jpg',
+         './../../assets/images/skittlesWildBerry.jpg',
+         './../../assets/images/snickers.jpg',
+         './../../assets/images/snydersHoneyMustardPieces.jpg',
+         './../../assets/images/threeMuskateers.jpg',
+         './../../assets/images/trailMix.jpg',
+         './../../assets/images/twix.jpg',
+         './../../assets/images/zooCrackers.jpg'
         ];
 
         $scope.throwout = function (eventName, eventObject) {
