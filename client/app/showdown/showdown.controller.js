a394aaea2a44fd2555052a0bf0618c5f87db32c7
nick <njgheorghita@email.wm.edu>
2014-12-11 00:50:14 -0600

    basic polling functionality added

--- client/app/showdown/showdown.controller.js
+++ client/app/showdown/showdown.controller.js
@@ -2,19 +2,36 @@
 
 angular.module('snckcoApp')
 
-	.controller('ShowdownCtrl', function ($scope) {
+
+
+
+	.directive('myContinent', function () {
+		return {
+			restrict: 'E',
+			template: '<div class="col-md-4"> <a class="btn col-md-12" ng-click="target=continentId" ng-class="{active : target == continentId}"><img</div>',
+			scope: {
+				continentId: '@',
+				showdowns: '@',
+				target: '='
+			}
+		};
+	})
+
+	
+.controller('ShowdownCtrl', function ($scope) {
+
 		$scope.showdowns = [
 		'./../../assets/images/showdown1.jpg',
 		'./../../assets/images/showdown2.jpg',
 		'./../../assets/images/showdown3.png',
 		'./../../assets/images/showdown4.jpg'
-		]
-
-		$scope.toggle = function (vote) {
-			vote.selected = !vote.selected;
-		};
+		];
 
 
+
+		$scope.myDropDown = 'first';
+		$scope.continent = 0;
 	});
+
 
 
